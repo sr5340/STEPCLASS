b57e4bdf9f375978ed03898b949b2d607eddf215
sr5340 <sr5340@srmist.edu.in>
2026-03-03 06:01:33 +0000

    Add BannerApp3 class with main method for banner display

---
+++ BannerApp3.java
@@ -0,0 +1,12 @@
+public class BannerApp3 {
+	public static void main(String[] args){
+		System.out.println("...");		   
+		System.out.println(String.join(" ","   ***   " , " ", "   ***   " , " ", " ******  " , " ", "  *****  "));
+		System.out.println(String.join(" "," **   ** " , " ", " **   ** " , " ", " **    **" , " ", " **   ** "));
+		System.out.println(String.join(" ","**     **" , " ", "**     **" , " ", " **    **" , " ", " **      "));
+		System.out.println(String.join(" ","**     **" , " ", "**     **" , " ", " ******  " , " ", "  *****  "));
+		System.out.println(String.join(" ","**     **" , " ", "**     **" , " ", " **      " , " ", "      ** "));
+		System.out.println(String.join(" "," **   ** " , " ", " **   ** " , " ", " **      " , " ", " **   ** "));
+		System.out.println(String.join(" ","   ***   " , " ", "   ***   " , " ", " **      " , " ", "  *****  "));
+	}
+}
