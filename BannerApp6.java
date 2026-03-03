52474797728cfb7c2194e20f28e994845d062cf8
sr5340 <sr5340@srmist.edu.in>
2026-03-03 06:08:09 +0000

    Add BannerApp6 class with methods for O and P pattern display

---
+++ BannerApp6.java
@@ -0,0 +1,29 @@
+public public class BannerApp6 {
+    public static String[] getOPattern() {
+        return new String[] {
+            "   ***   ",
+            " **   ** ",
+            "**     **",
+            "**     **",
+            "**     **",
+            "**     **",
+            "**     **",
+            " **   ** ",
+            "   ***   "
+        };
+    }
+    public static String[] getPPattern() {
+        return new String[] {
+            " ******  ",
+            " **    **",
+            " **    **",
+            " ******  ",
+            " **      ",
+            " **      ",
+            " **      ",
+            " **      ",
+            " **      "
+        };
+    } {
+    
+}
